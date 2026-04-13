57d1cc40aebb99a771ae95bad5724b5318cf0467
Gyenge Elemér <gyenge@valahol>
2026-04-13 20:05:43 +0200

    Fájlkezelés előkészítve

---
+++ src/main/java/com/example/Store.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Store {
+    public static void write(String line) {
+        try {
+            tryWrite(line);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    private static void tryWrite(String line) throws IOException {
+        FileWriter fileWriter = new FileWriter("adat.txt", true);
+        fileWriter.write(line);
+        fileWriter.write("\n");
+        fileWriter.close();
+    }
+}
